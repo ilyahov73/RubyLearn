410e46b3b9b4c68f17cdf18f9aed37003efd0725
ilyahov73 <56978420+ilyahov73@users.noreply.github.com>
2021-11-09 00:39:52 +0400

    script.js fix2

--- java_script/script.js
+++ java_script/script.js
@@ -2,9 +2,7 @@ var button, field , count = 0;
 
 function  move (num_of_button){     
     button = document.getElementById("id_" + num_of_button).innerHTML; 
-    if(button === "") {
-        document.getElementById("id_" + num_of_button).innerHTML  = "X"; 
-    }
+    if( button === "") {document.getElementById("id_" + num_of_button).innerHTML  = "X"; }
     count += 1; 
     winner();      
     computer_turn(); 
@@ -25,8 +23,8 @@ function winner(){
         [2, 4, 6]
     ]
     for (index_of_line = 0; index_of_line < lines.length; index_of_line++){
-        if(field[lines[index_of_line][0]].innerHTML =='X' & field[lines[index_of_line][1]].innerHTML =='X' & field[lines[index_of_line][2]].innerHTML =='X'){alert('Крестики победили!');  fail;} 
-        if(field[lines[index_of_line][0]].innerHTML =='0' & field[lines[index_of_line][1]].innerHTML =='0' & field[lines[index_of_line][2]].innerHTML =='0'){alert('Нолики победили!');  fail;} 
+        if(field[lines[index_of_line][0]].innerHTML ==='X' & field[lines[index_of_line][1]].innerHTML ==='X' & field[lines[index_of_line][2]].innerHTML ==='X'){alert('Крестики победили!');  fail;} 
+        if(field[lines[index_of_line][0]].innerHTML ==='0' & field[lines[index_of_line][1]].innerHTML ==='0' & field[lines[index_of_line][2]].innerHTML ==='0'){alert('Нолики победили!');  fail;} 
     }
     
     if(count == "9") { alert('Ничья!');   } 
@@ -38,7 +36,7 @@ function computer_turn(){
     array_of_empty_cell = [];
     
     for (index = 0; index < field.length; ++index) {
-        if (field[index].innerHTML != 'X' & field[index].innerHTML != '0'){array_of_empty_cell.push(index)}
+        if (field[index].innerHTML !== 'X' & field[index].innerHTML !== '0'){array_of_empty_cell.push(index)}
     }
     document.getElementById("id_" + array_of_empty_cell[Math.floor(Math.random() * array_of_empty_cell.length)]).innerHTML  = "0"; 
     count += 1; 
