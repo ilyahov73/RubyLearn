843da92e4fad028f4217470dfcc0279b31e62968
ilyahov73 <56978420+ilyahov73@users.noreply.github.com>
2021-11-09 00:25:42 +0400

    script.js fix

--- java_script/script.js
+++ java_script/script.js
@@ -1,6 +1,5 @@
 var button, field , count = 0;
 
-
 function  move (num_of_button){     
     button = document.getElementById("id_" + num_of_button).innerHTML; 
     if(button === "") {
@@ -29,30 +28,8 @@ function winner(){
         if(field[lines[index_of_line][0]].innerHTML =='X' & field[lines[index_of_line][1]].innerHTML =='X' & field[lines[index_of_line][2]].innerHTML =='X'){alert('Крестики победили!');  fail;} 
         if(field[lines[index_of_line][0]].innerHTML =='0' & field[lines[index_of_line][1]].innerHTML =='0' & field[lines[index_of_line][2]].innerHTML =='0'){alert('Нолики победили!');  fail;} 
     }
-    // if(field[0].innerHTML =='X' & field[1].innerHTML =='X' & field[2].innerHTML =='X'){alert('Крестики победили!');  fail;} 
-    // if(field[3].innerHTML =='X' & field[4].innerHTML =='X' & field[5].innerHTML =='X'){alert('Крестики победили!');  fail;}  
-    // if(field[6].innerHTML =='X' & field[7].innerHTML =='X' & field[8].innerHTML =='X'){alert('Крестики победили!');  fail;}
-
-    // if(field[0].innerHTML =='X' & field[3].innerHTML =='X' & field[6].innerHTML =='X'){alert('Крестики победили!');  fail;} 
-    // if(field[1].innerHTML =='X' & field[4].innerHTML =='X' & field[7].innerHTML =='X'){alert('Крестики победили!');  fail;}  
-    // if(field[2].innerHTML =='X' & field[5].innerHTML =='X' & field[8].innerHTML =='X'){alert('Крестики победили!');  fail;}
-
-    // if(field[0].innerHTML =='X' & field[4].innerHTML =='X' & field[8].innerHTML =='X'){alert('Крестики победили!');  fail;} 
-    // if(field[2].innerHTML =='X' & field[4].innerHTML =='X' & field[6].innerHTML =='X'){alert('Крестики победили!');  fail;}  
-
-    // if(field[0].innerHTML =='0' & field[1].innerHTML =='0' & field[2].innerHTML =='0'){alert('Нолики победили!');  fail;} 
-    // if(field[3].innerHTML =='0' & field[4].innerHTML =='0' & field[5].innerHTML =='0'){alert('Нолики победили!');  fail;}  
-    // if(field[6].innerHTML =='0' & field[7].innerHTML =='0' & field[8].innerHTML =='0'){alert('Нолики победили!');  fail;}
-
-    // if(field[0].innerHTML =='0' & field[3].innerHTML =='0' & field[6].innerHTML =='0'){alert('Нолики победили!');  fail;} 
-    // if(field[1].innerHTML =='0' & field[4].innerHTML =='0' & field[7].innerHTML =='0'){alert('Нолики победили!');  fail;}  
-    // if(field[2].innerHTML =='0' & field[5].innerHTML =='0' & field[8].innerHTML =='0'){alert('Нолики победили!');  fail;}
-
-    // if(field[0].innerHTML =='0' & field[4].innerHTML =='0' & field[8].innerHTML =='0'){alert('Нолики победили!');  fail;} 
-    // if(field[2].innerHTML =='0' & field[4].innerHTML =='0' & field[6].innerHTML =='0'){alert('Нолики победили!');  fail;}
-
-    if(count == "9") { alert('Ничья!');   } 
     
+    if(count == "9") { alert('Ничья!');   } 
 }
 
 function computer_turn(){
